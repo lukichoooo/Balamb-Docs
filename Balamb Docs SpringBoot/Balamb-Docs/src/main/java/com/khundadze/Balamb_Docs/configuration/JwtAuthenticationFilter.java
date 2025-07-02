08197dbc9fc42a80a20cf84c29aca16531225378
Luka <khundadzeluka702@gmail.com>
2025-07-02 22:00:58 +0400

    started adding JWT auth

--- Balamb Docs SpringBoot/Balamb-Docs/src/main/java/com/khundadze/Balamb_Docs/configuration/JwtAuthenticationFilter.java
+++ Balamb Docs SpringBoot/Balamb-Docs/src/main/java/com/khundadze/Balamb_Docs/configuration/JwtAuthenticationFilter.java
@@ -11,13 +11,30 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain)
             throws ServletException, IOException {
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authorizationHeader.substring("Bearer ".length());
+
+        String token = authorizationHeader.substring("Bearer ".length());
+        jwtService.validateToken(token, null); // TODO add user
+        filterChain.doFilter(request, response);
     }
 
 }
