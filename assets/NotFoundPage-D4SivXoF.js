50e60a53ed50cf57236d0979ec6676269d21e9d4
Luka <khundadzeluka702@gmail.com>
2025-07-30 13:56:50 +0400

    Updates

--- assets/NotFoundPage-D4SivXoF.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CfaWrpPQ.js";function n(){return o.jsxs("div",{children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx("p",{children:"The page you are looking for does not exist."})]})}export{n as default};
